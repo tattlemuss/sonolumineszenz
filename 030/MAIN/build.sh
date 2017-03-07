ec2b5817eb44c031492253fde5620219a2cabeae
Steve (laptop) <dactyl_s@bitbucket.org>
2017-03-07 20:51:23 +0000

    Changed build to use vlink so it outputs symbols (to verify where data is currently loaded)

--- 030/MAIN/build.sh
+++ 030/MAIN/build.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env sh
-vasmm68k_mot -Ftos -devpac -nocase -m68030 -I .. -I ../../mdata/ -I ../CIA/ -I ../../ -I ../../DATA/ MAIN_1.S
+vasmm68k_mot -Felf -devpac -nocase -m68030 -I .. -I ../../mdata/ -I ../CIA/ -I ../../ -I ../../DATA/ MAIN_1.S -o MAIN_1.O
+
+vlink MAIN_1.O -s -M -b ataritos -o SL.PRG > syms.txt
+
