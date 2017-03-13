45e61281db5bdbcfd8a48e6e28c7273975569b5a
Steve (laptop) <dactyl_s@bitbucket.org>
2017-03-13 20:18:29 +0000

    Added linkfiling to the build process.

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env sh
-vasmm68k_mot -Felf -devpac -nocase -m68030 -I .. -I ../../mdata/ -I ../CIA/ -I ../../ -I ../../DATA/ MAIN_1.S -o MAIN_1.O
 
+# Build the main prg
+vasmm68k_mot -Felf -devpac -nocase -m68030 -I ..  -I 030/MAIN -I 030/CIA/ -I 030 -I DATA/ 030/MAIN/MAIN_1.S -o MAIN_1.O
 vlink MAIN_1.O -s -M -b ataritos -o SL.PRG > syms.txt
+
+# Build the WAD tool
+cd tools/unlinker && make all
+
+# Build the WAD
+cd ../..
+tools/unlinker/bin/linkfile unpacked/*.PRG unpacked/*.AON
 
