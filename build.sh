89d31e0b259301e1daa67dcc87e9d52e053da9f0
Steve (laptop) <dactyl_s@bitbucket.org>
2017-03-13 20:39:48 +0000

    Ensure intermediate/built data gets cleaned

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 
+# Clean
+rm SL.LNK
+rm SL.PRG
+rm *.O
+
 # Build the main prg
 vasmm68k_mot -Felf -devpac -nocase -m68030 -I ..  -I 030/MAIN -I 030/CIA/ -I 030 -I DATA/ 030/MAIN/MAIN_1.S -o MAIN_1.O
 vlink MAIN_1.O -s -M -b ataritos -o SL.PRG > syms.txt
